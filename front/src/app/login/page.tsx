ada3a0208adf82210fbf49de67d2bd4f5b9ace8b
germanreynoso <germanreynoso94@gmail.com>
2024-10-06 20:56:26 -0300

    cambios para diploy

--- front/src/app/login/page.tsx
+++ front/src/app/login/page.tsx
@@ -1,9 +1,8 @@
-
 import Login from "@/components/Login/Login";
 import { ILogin } from "@/interfaces/LoginRegister";
 import React from "react";
 
-const login = (dataUser?: ILogin) => {
+const login = (dataUser: ILogin = { /* valores por defecto */ }) => {
   return (
     <div>
       <Login />
